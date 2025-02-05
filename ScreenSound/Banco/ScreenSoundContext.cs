96ff0ea9f39725293acec6979d1a490ea8551042
Arthur Menezes <arthur.menezes2003@hotmail.com>
2025-02-05 01:05:12 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco;
+internal class ScreenSoundContext : DbContext
+{
+    public DbSet<Artista> Artistas;
+
+    private string connectionString = "Data Source=DESKTOP-SD3LB7D;" +
+        "Initial Catalog=ScreenSound;" +
+        "Integrated Security=True;" +
+        "Encrypt=True;" +
+        "Trust Server Certificate=True";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
